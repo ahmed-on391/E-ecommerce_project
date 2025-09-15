491db0bc590f0b7c5b9da74a21b8f5934c44a012
ahemd-one <ahmededress111.gmail.com>
2025-09-15 06:10:19 +0300

    Added frontend template and updated HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,17 @@ class HomeController extends Controller
     {
         return view('admin.index');
     }
+#---------------------------------------------------------------------------
 
+
+
+    public function home()
+    {
+        return view('home.index');
+    }
+
+
+    #---------------------------------------------------------------------------
     /**
      * Show the form for creating a new resource.
      */
